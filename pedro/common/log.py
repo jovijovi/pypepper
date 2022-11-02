fb7c133d9248f8ba09d389295460b1f735f36e4e
jovijovi <mageyul@hotmail.com>
2022-11-02 18:26:22 +0800

    feat(common/log): a simple logger in Pedro style

---
+++ pedro/common/log.py
@@ -0,0 +1,98 @@
+import json
+import os
+import socket
+import sys
+from string import Template
+from typing import Any
+
+from loguru import logger
+
+# Log format
+log_fmt = "[<green>{time:YYYY-MM-DDTHH:mm:ss.SSSZ}</green>][<level>{level:<8}</level>][<cyan>$host:$user</cyan>]" \
+          "[<cyan>pid:{process}|tid:{thread}</cyan>][<cyan>{file.path}:{line}</cyan>]" \
+          "[<cyan>{module}.{function}</cyan>][<magenta>{extra[req_id]}</magenta>][<level>{message}</level>]"
+
+# Template of log format
+log_format_template = Template(log_fmt).substitute(host=socket.gethostname(), user=os.getlogin())
+
+# Log default config
+config = {
+    "handlers": [
+        {
+            "sink": sys.stdout,
+
+            # Log format
+            "format": log_format_template,
+
+            # Default log level
+            "level": "TRACE",
+
+            # Adds colors to logs
+            "colorize": True,
+
+            # Enqueue the messages to ensure logs integrity
+            # Ref: https://loguru.readthedocs.io/en/stable/overview.html#asynchronous-thread-safe-multiprocess-safe
+            "enqueue": True,
+        },
+    ],
+    "levels": [
+        dict(name="FATAL", no=60, icon="☠", color="<RED><bold>"),
+    ],
+    "extra": {
+        # Default request ID
+        "req_id": 0,
+    },
+}
+
+logger.remove()
+logger.configure(**config)
+
+version = {
+    "version": "unknown",
+    "gitCommit": "unknown",
+    "buildTime": "unknown",
+    "pythonVersion": "unknown"
+}
+
+
+class Logger:
+    def __init__(self):
+        self._logger = logger.opt(depth=1)
+
+    def get_logger(self):
+        return self._logger
+
+    def request_id(self, req_id=0):
+        self._logger = self._logger.bind(req_id=req_id)
+        return self
+
+    def logo(self, msg: str):
+        msg += json.dumps(version, indent=4)
+        self._logger.info(msg)
+
+    # Severity: 5
+    def trace(self, msg: str, *args: Any, **kwargs: Any):
+        self._logger.trace(msg, *args, **kwargs)
+
+    # Severity: 10
+    def debug(self, msg: str, *args: Any, **kwargs: Any):
+        self._logger.debug(msg, *args, **kwargs)
+
+    # Severity: 20
+    def info(self, msg: str, *args: Any, **kwargs: Any):
+        self._logger.info(msg, *args, **kwargs)
+
+    # Severity: 30
+    def warn(self, msg: str, *args: Any, **kwargs: Any):
+        self._logger.warning(msg, *args, **kwargs)
+
+    # Severity: 40
+    def error(self, msg: str, *args: Any, **kwargs: Any):
+        self._logger.error(msg, *args, **kwargs)
+
+    # Severity: 60
+    def fatal(self, msg: str, *args: Any, **kwargs: Any):
+        self._logger.log("FATAL", msg, *args, **kwargs)
+
+
+log = Logger()
---
+++ tests/common/test_common_log.py
@@ -0,0 +1,45 @@
+import json
+import unittest
+
+from pedro.common.log import log
+from pedro.logo import logo
+
+
+class MyTestCase(unittest.TestCase):
+    @staticmethod
+    def test_log():
+        log.logo(logo)
+
+        log.trace("TRACE without reqID")
+        log.debug("DEBUG without reqID")
+        log.info("INFO without reqID")
+        log.warn("WARN without reqID")
+        log.error("ERROR without reqID")
+        log.fatal("FATAL without reqID")
+
+        log.request_id("reqID-1").trace("This is a TRACE msg")
+        log.request_id("reqID-2").debug("This is a DEBUG msg")
+        log.request_id("reqID-3").info("This is a INFO msg")
+        log.request_id("reqID-4").warn("This is a WARN msg")
+        log.request_id("reqID-5").error("This is a ERROR msg")
+        log.request_id("reqID-6").fatal("This is a FATAL msg")
+
+        word = "world"
+        log.request_id(6).info("Hello, world!")
+        log.request_id(7).info("Hello, {}!", word)
+        log.request_id(8).info('Hello, {}!', word)
+        log.request_id(9).info("""Hello, {}!""", word)
+        log.request_id(10).info("{}, {}!", "Hello", "world")
+
+        user = {
+            "Name": "Jerry",
+            "Age": 1,
+            "Score": [0, 2, 4],
+            "Files": {"foo": 1},
+        }
+
+        log.request_id().debug(json.dumps(user, indent=4))
+
+
+if __name__ == '__main__':
+    unittest.main()
