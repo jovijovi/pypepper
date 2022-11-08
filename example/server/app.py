6437bd3df2e43459c00fa893d67cb46ff0603192
jovijovi <mageyul@hotmail.com>
2022-11-08 11:55:47 +0800

    feat(example/server): run HTTP server

--- example/server/app.py
+++ example/server/app.py
@@ -1,27 +1,8 @@
-import json
-
-from flask import Flask
-
 from pedro.common import system
 from pedro.common.config import config
 from pedro.common.log import log
 from pedro.logo import logo
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    """A multi-line
-    docstring.
-    """
-    return 'Hello World!'
-
-
-@app.route('/health')
-def health():  # put application's code here
-    log.request_id().debug("health")
-    return json.dumps("health")
+from pedro.network.http import server
 
 
 def main():
@@ -29,7 +10,7 @@ def main():
     system.handle_signals()
     config.load_config()
 
-    app.run(host='0.0.0.0', port=55550)
+    server.run()
 
 
 if __name__ == '__main__':
