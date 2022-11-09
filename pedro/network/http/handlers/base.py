f7ff1adcbaa5bf35a1b027fc1ded74653e0370ea
jovijovi <mageyul@hotmail.com>
2022-11-09 15:52:20 +0800

    feat(network/http): build response in Pedro style

--- pedro/network/http/handlers/base.py
+++ pedro/network/http/handlers/base.py
@@ -1,15 +1,16 @@
-import json
+from flask import request
 
 from pedro.common.log import log
 from pedro.common.version import version
+from pedro.network.http import response
 
 
 def health():
-    log.request_id().debug("health")
-    return json.dumps(version.get_version_info())
+    log.request_id().trace("Receive HealthCheck. URL.Path={}", request.path)
+    return response.build_response(code="200", data=version.get_version_info(), msg="OK")
 
 
 # TODO:
 def metrics():
-    log.request_id().debug("metric")
-    return json.dumps(version.get_version_info())
+    log.request_id().info("Receive MetricsCheck. URL.Path={}", request.path)
+    return response.build_response(code="200", data="metrics", msg="OK")
