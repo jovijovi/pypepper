cfe1b4a475fb1da64042c2d5d646572a3ae25ce7
jovijovi <mageyul@hotmail.com>
2022-11-11 17:07:19 +0800

    feat(common/utils/retry): - support lambda - random retry interval

--- pedro/common/utils/retry.py
+++ pedro/common/utils/retry.py
@@ -1,22 +1,40 @@
-from typing import Any
+from typing import Callable, TypeVar, ParamSpec
 
 from pedro.common.log import log
 from pedro.common.utils import time
+from pedro.common.utils.random import random
+from pedro.exceptions import InternalException
+
+DEFAULT_RETRY_TIMES: int = 3
+DEFAULT_RETRY_INTERVAL: int = 3
+DEFAULT_RETRY_MIN_INTERVAL: int = DEFAULT_RETRY_INTERVAL
+DEFAULT_RETRY_MAX_INTERVAL: int = DEFAULT_RETRY_MIN_INTERVAL * 3
+
+T = TypeVar("T")
+P = ParamSpec("P")
 
 
-def run(f, retry_times=3, retry_interval=3, verbose_log=True) -> Any:
+def run(
+        func: Callable[P, T],
+        retry_times: int = DEFAULT_RETRY_TIMES,
+        retry_interval: int = DEFAULT_RETRY_INTERVAL,
+        verbose_log: bool = True,
+) -> T:
     """
     Retry running the function m times with n seconds interval
-    :param f: function
+    :param func: function
     :param retry_times: retry times
     :param retry_interval: retry interval
     :param verbose_log: verbose log
     :return: function result
     """
+    if not func:
+        raise InternalException("invalid function")
+
     for i in range(retry_times):
         try:
-            return f()
-        except Exception as e:
+            return func()
+        except InternalException as e:
             if verbose_log:
                 log.error('Try times={}, error={}', i + 1, e)
 
@@ -24,3 +42,10 @@ def run(f, retry_times=3, retry_interval=3, verbose_log=True) -> Any:
                 raise e
 
             time.sleep(second=retry_interval)
+
+
+def random_retry_interval(
+        min_interval: int = DEFAULT_RETRY_MIN_INTERVAL,
+        max_interval: int = DEFAULT_RETRY_MAX_INTERVAL,
+) -> int:
+    return random.rand_uint_between(min_interval, max_interval)
--- tests/common/test_common_utils_retry.py
+++ tests/common/test_common_utils_retry.py
@@ -1,20 +1,84 @@
 import pytest
 
 from pedro.common.utils import retry
+from pedro.exceptions import InternalException
 
 
-def hello_world() -> int:
+def transistor(in_voltage: int) -> int:
+    voltage = 1
+    delta = in_voltage - voltage
+    if delta > 0:
+        raise InternalException("too high")
+    elif delta < 0:
+        raise InternalException("too low")
+
+    return in_voltage
+
+
+def hello_world() -> None:
     print("Hello, world!")
-    return 42
 
 
-def test_retry():
+def answer(arg: int = 42) -> int:
+    return arg
+
+
+def test_retry_simple():
+    retry.run(func=hello_world)
+
+
+def test_retry_with_default_params():
+    result = retry.run(func=answer, retry_times=3, retry_interval=5, verbose_log=False)
+    print(f"The Answer is {result}, Type={type(result)}")
+    assert isinstance(result, int)
+    assert result == 42
+
+
+def test_retry_lambda():
+    def say(words: str) -> str:
+        return words
+
+    result1 = retry.run(func=lambda: say("hi"), retry_times=3, retry_interval=1)
+    print(f'Say "{result1}"')
+
+    result2 = retry.run(func=lambda words='hi': say(words), retry_times=3, retry_interval=1)
+    print(f'Say "{result2}"')
+
+    result3 = retry.run(func=lambda: answer(0), retry_times=3, retry_interval=1)
+    print(f'The Answer is "{result3}"')
+
+
+def test_transistor():
     try:
-        result = retry.run(hello_world, retry_times=3, retry_interval=1, verbose_log=False)
-        print(result)
-        assert result == 42
-    except Exception as e:
+        result = retry.run(func=lambda: transistor(0))
+        print("Result=", result)
+    except InternalException as e:
         print("Expected error=", e)
+
+    try:
+        result = retry.run(
+            func=lambda: transistor(2),
+            retry_times=2,
+            retry_interval=retry.random_retry_interval(),
+            verbose_log=False,
+        )
+        print("Result=", result)
+    except InternalException as e:
+        print("Expected error=", e)
+
+    try:
+        result = retry.run(func=lambda: transistor(42))
+        print("Result=", result)
+        assert result == 42
+    except InternalException as e:
+        print(e)
+
+
+def test_invalid_func():
+    try:
+        retry.run(None)
+    except InternalException as e:
+        print(e)
 
 
 if __name__ == '__main__':
