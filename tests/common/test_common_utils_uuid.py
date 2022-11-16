9a43b29a8e026e40a3c0da58f8cbba2b5b15e7d9
jovijovi <mageyul@hotmail.com>
2022-11-16 19:45:21 +0800

    test(common/utils/uuid): more iterative testing

--- tests/common/test_common_utils_uuid.py
+++ tests/common/test_common_utils_uuid.py
@@ -4,21 +4,21 @@ from pedro.common.utils import uuid
 
 
 def test_new_uuid():
-    for i in range(3):
+    for i in range(10):
         result = uuid.new_uuid()
         print("UUID=", result)
         assert len(result) == 32 + 4
 
 
 def test_new_uuid_32bits():
-    for i in range(3):
+    for i in range(10):
         result = uuid.new_uuid_32bits()
         print("UUID=", result)
         assert len(result) == 32
 
 
 def test_custom_uuid():
-    for i in range(3):
+    for i in range(10):
         result = uuid.custom_uuid(6)
         print("UUID=", result)
         assert len(result) == 6
