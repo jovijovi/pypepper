2f7d7f25b929f0999a64f1babbae5e8abcfe024c
jovijovi <mageyul@hotmail.com>
2022-11-18 15:54:24 +0800

    test(fsm): more test code

--- tests/fsm/test_fsm.py
+++ tests/fsm/test_fsm.py
@@ -1,5 +1,6 @@
 import pytest
 
+from pedro import errors
 from pedro.event import event
 from pedro.fsm import fsm
 
@@ -7,27 +8,44 @@ from pedro.fsm import fsm
 def test_new_state():
     state = fsm.State('Closed')
     print("State=", state.value)
+    assert state.value == 'Closed'
+
     state.value = 'Opened'
     print("State=", state.value)
+    assert state.value == 'Opened'
 
 
 def test_fsm():
+    # New event 1
     evt1 = event.new()
     evt1.set_name('OpenDoor')
     evt1.set_src('Closed')
+    assert evt1
+    assert evt1.data.name == 'OpenDoor'
+    assert evt1.data.src == 'Closed'
 
+    # New event 2
     evt2 = event.new()
     evt2.set_name('CloseDoor')
     evt2.set_src('Opened')
+    assert evt2
+    assert evt2.data.name == 'CloseDoor'
+    assert evt2.data.src == 'Opened'
 
+    # New event 3
     evt3 = event.new()
     evt3.set_name('Knock Knock')
     evt3.set_src('Closed')
+    assert evt3
+    assert evt3.data.name == 'Knock Knock'
+    assert evt3.data.src == 'Closed'
 
+    # Define an event handler
     def event2_handler():
         print("Event2 transition's handler: somebody close the door")
         return "#42#"
 
+    # Build FSM options
     options = fsm.Options(
         fsm_id='test-id-1',
         initial=fsm.State('Closed'),
@@ -51,14 +69,23 @@ def test_fsm():
             )
         ]
     )
-    machine = fsm.new(options)
+    assert options
+    assert options.fsm_id == 'test-id-1'
+    assert options.initial.value == 'Closed'
+    assert len(options.transitions) == 2
 
+    # New an FSM
+    machine = fsm.new(options)
+    assert machine
     print("Initial state=", machine.current().value)
+    assert machine.current().value == 'Closed'
 
     # Open the door
     rsp1 = machine.on(evt1, lambda: print(f'Event({evt1.data.name}) finished'))
     print("Error1=", rsp1.error)
+    assert rsp1.error is None
     print("Current state1=", rsp1.state.value)
+    assert rsp1.state.value == 'Opened'
 
     # Close the door
     rsp2 = machine.on(
@@ -70,14 +97,20 @@ def test_fsm():
         }
     )
     print("Error2=", rsp2.error)
+    assert rsp2.error is None
     print("Event handler result=", rsp2.event_handler_result)
+    assert rsp2.event_handler_result == '#42#'
     print("Current state2=", rsp2.state.value)
+    assert rsp2.state.value == 'Closed'
 
     # Knock Knock
     rsp3 = machine.on(evt3)
     print("Error3=", rsp3.error)
+    assert str(rsp3.error) == errors.ERROR_INVALID_EVENT
     print("Transition result=", rsp3.transition_result)
+    assert rsp3.transition_result is None
     print("Current state3=", rsp3.state.value)
+    assert rsp3.state.value == 'Closed'
 
     # Open the door again
     def mock_error(ctx):
@@ -89,13 +122,18 @@ def test_fsm():
         'what': 'throw',
     })
     print("Error4=", rsp4.error)
+    assert str(rsp4.error) == 'A mock Error!'
     print("Current state4=", rsp4.state.value)
+    assert rsp4.state.value == 'Opened'
 
-    # Knock Knock
+    # Knock Knock again
     rsp5 = machine.on(evt2, lambda: 42)
     print("Error5=", rsp5.error)
+    assert rsp5.error is None
     print("Transition result=", rsp5.transition_result)
+    assert rsp5.transition_result == 42
     print("Current state5=", rsp5.state.value)
+    assert rsp5.state.value == 'Closed'
 
     # Close FSM
     machine.close()
