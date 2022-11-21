cd091ef5cd59166fda09a5b211520385b960f173
jovijovi <mageyul@hotmail.com>
2022-11-21 18:05:00 +0800

    build: remove dist dir at first

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import shutil
 
+# Remote dist dir
+shutil.rmtree("./dist")
+
+# Copy code
 shutil.copytree("./conf", "./dist/conf")
 shutil.copytree("./pypedro", "./dist/pypedro")
 shutil.copytree("./example", "./dist/example")
-shutil.copyfile("./git.json", "./dist/git.json")
