01804402af3adcf11688ac21343eb60a6c5c0647
jovijovi <mageyul@hotmail.com>
2022-12-21 19:19:03 +0800

    feat(scheduler): a job status FSM

--- tests/scheduler/test_scheduler.py
+++ tests/scheduler/test_scheduler.py
@@ -2,6 +2,8 @@ import time
 
 import pytest
 
+from pypedro.scheduler import events
+from pypedro.scheduler.events import FSM
 from pypedro.scheduler.job import Job
 
 
@@ -11,6 +13,33 @@ def test_scheduler():
         job.channel_id = 'bar'
         job.scheduled()
     time.sleep(1)
+
+
+def test_status():
+    try:
+        machine = FSM()
+
+        status1 = machine.on(events.INIT)
+        print("Status=", status1.state.value)
+
+        status1 = machine.on(events.INIT)
+        assert status1.error
+        print("Error=", status1.error)
+
+        status1 = machine.on(events.SCHEDULE)
+        print("Status=", status1.state.value)
+
+        status1 = machine.on(events.RUN)
+        print("Status=", status1.state.value)
+
+        status1 = machine.on(events.COMPLETE)
+        print("Status=", status1.state.value)
+
+        status1 = machine.on(events.FAIL)
+        assert status1.error
+        print("Error=", status1.error)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
